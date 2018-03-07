a4732f7a576108cf943f1ff658d802f1fc18b391
Michael Palhidai <mpalhidai@gmail.com>
2018-03-07 10:26:30 -0800

    Writing test procedures

--- functions.rb
+++ functions.rb
@@ -7,5 +7,11 @@ check each item with the next item
 if the item is greater than the one to the right, swap the items
 loop through the array until no swapping has occured
 =end
+  array.sort!
 
 end
+
+p bubble_sort([1,2,3,4,5]) == [1, 2, 3, 4, 5] ? "Test 1: Passed" : "Test 2: Failed"
+p true if bubble_sort([2,5,4,3,1]) == [1, 2, 3, 4, 5]
+p true if bubble_sort([1,5,3,4,5]) == [1, 3, 4, 5, 5]
+p true if bubble_sort([1,-5,3,-4,5]) == [-5, -4, 1, 3, 5]
