3629d01af40ba39dc623d9af47f5f64207e39fe1
Michael Palhidai <mpalhidai@gmail.com>
2018-03-07 13:04:35 -0800

    Fixed bubble sort

--- functions.rb
+++ functions.rb
@@ -7,18 +7,18 @@ check each item with the next item
 if the item is greater than the one to the right, swap the items
 loop through the array until no swapping has occured
 =end
-  
-  sorted = false
-  while !sorted
+
+  sorted = 1
+  while sorted > 0
+    sorted = 0
     array.each_index do |i|
-      sorted = true
       if array[i+1] != nil && array[i] > array[i+1]
         array[i], array[i+1] = array[i+1], array[i]
-        sorted = false
+        sorted+=1
       end
     end
   end
-  p array
+  return array
 end
 
 
@@ -27,5 +27,5 @@ p bubble_sort([2,5,4,3,1]) == [1, 2, 3, 4, 5] ? "Test 2: Passed" : "Test 2: Fail
 p bubble_sort([1,5,3,4,5]) == [1, 3, 4, 5, 5] ? "Test 3: Passed" : "Test 3: Failed"
 p bubble_sort([1,-5,3,-4,5]) == [-5, -4, 1, 3, 5] ? "Test 4: Passed" : "Test 4: Failed"
 
-#randomArray = 999.times.map{Random.rand(-99999..99999)}
-#p bubble_sort(randomArray) == randomArray.sort ? "Test 5: Passed" : "Test 5: Failed"
+randomArray = 999.times.map{Random.rand(-99999..99999)}
+p bubble_sort(randomArray) == randomArray.sort ? "Test 5: Passed" : "Test 5: Failed"
