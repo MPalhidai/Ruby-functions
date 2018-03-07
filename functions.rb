22fa73cf06b74886c9bc26d82e815eb6c838d144
Michael Palhidai <mpalhidai@gmail.com>
2018-03-07 10:15:55 -0800

    Added bubble sort comments

--- functions.rb
+++ functions.rb
@@ -1,5 +1,11 @@
 
 
-def bubble_sort()
+def bubble_sort(array)
+=begin
+loop through each item
+check each item with the next item
+if the item is greater than the one to the right, swap the items
+loop through the array until no swapping has occured
+=end
 
 end
