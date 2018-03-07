8afd0e7f752031b25ccf33b264696fe8668b213a
Michael Palhidai <mpalhidai@gmail.com>
2018-03-07 10:50:09 -0800

    Added Test Scripts

--- functions.rb
+++ functions.rb
@@ -11,7 +11,11 @@ loop through the array until no swapping has occured
 
 end
 
-p bubble_sort([1,2,3,4,5]) == [1, 2, 3, 4, 5] ? "Test 1: Passed" : "Test 2: Failed"
-p true if bubble_sort([2,5,4,3,1]) == [1, 2, 3, 4, 5]
-p true if bubble_sort([1,5,3,4,5]) == [1, 3, 4, 5, 5]
-p true if bubble_sort([1,-5,3,-4,5]) == [-5, -4, 1, 3, 5]
+
+p bubble_sort([1,2,3,4,5]) == [1, 2, 3, 4, 5] ? "Test 1: Passed" : "Test 1: Failed"
+p bubble_sort([2,5,4,3,1]) == [1, 2, 3, 4, 5] ? "Test 2: Passed" : "Test 2: Failed"
+p bubble_sort([1,5,3,4,5]) == [1, 3, 4, 5, 5] ? "Test 3: Passed" : "Test 3: Failed"
+p bubble_sort([1,-5,3,-4,5]) == [-5, -4, 1, 3, 5] ? "Test 4: Passed" : "Test 4: Failed"
+
+randomArray = 999.times.map{Random.rand(-99999..99999)}
+p bubble_sort(randomArray) == randomArray.sort ? "Test 5: Passed" : "Test 5: Failed"
